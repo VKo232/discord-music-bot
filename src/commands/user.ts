72ce1a8482c8454bb552aef8d8eff246019a1d6a
Victor Ko <vk07275@gmail.com>
2023-02-25 01:29:49 -0500

    ooops

--- src/commands/user.ts
+++ src/commands/user.ts
@@ -1,20 +1,11 @@
 import {Client, Message, SlashCommandBuilder, }from 'discord.js';
-import { leaveChannel } from 'src/utils/bot/bot-service';
-
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('stop')
-		.setDescription('Plays a song!'),
+		.setName('user')
+		.setDescription('Provides information about the user.'),
 	async execute(client: Client, message: Message, args: any) {
-		if (!message.member?.voice?.channelId || !message.guild?.id || !message.guild?.voiceAdapterCreator ) {
-			await message.reply("this server does not have the required permissions");
-			return;
-		}
-
-		// call request bot to leave channel
-		leaveChannel(message.guild.id);
-		// clean up remove the queue
-		await message.reply("left channel");
-
+		// interaction.user is the object representing the User who ran the command
+		// interaction.member is the GuildMember object, which represents the user in the specific guild
+		await message.reply(`This command was run by ${message.author.username}, who joined on ${message?.member?.joinedAt}.`);
 	},
 };
