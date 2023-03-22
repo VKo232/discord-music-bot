b1e3a336b44f4a2c54f4235c44ab839bbd2107b6
Brandon Lew <brandonlew2000@gmail.com>
2023-03-22 14:34:57 -0400

    stops people from being idiots

--- src/commands/move.ts
+++ src/commands/move.ts
@@ -1,6 +1,6 @@
 import { Client, Message, SlashCommandBuilder } from "discord.js";
 import { emojiReact } from "../utils/bot/bot-service";
-import { hasPlayer, playerMove} from "../utils/player/player-service";
+import { hasPlayer, playerGetQueue, playerMove} from "../utils/player/player-service";
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -14,7 +14,8 @@ module.exports = {
     if (!message.guild?.id) {
       return;
     }
-    if (message.guild?.id && hasPlayer(message.guild?.id) && args.length == 1) {
+    const length = playerGetQueue(message.guild?.id).length;
+    if (message.guild?.id && hasPlayer(message.guild?.id) && args.length == 1 && parseInt(args[0]) - 1 <= length && parseInt(args[0]) > 0) {
         emojiReact({message,emoji:"ok_hand"});
         playerMove(message.guild?.id, parseInt(args[0]));
     } else {
--- src/commands/remove.ts
+++ src/commands/remove.ts
@@ -1,6 +1,6 @@
 import { Client, Message, SlashCommandBuilder } from "discord.js";
 import { emojiReact } from "../utils/bot/bot-service";
-import { hasPlayer, playerRemove} from "../utils/player/player-service";
+import { hasPlayer, playerRemove, playerGetQueue} from "../utils/player/player-service";
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -9,12 +9,13 @@ module.exports = {
   aliases: ["rm"],
   async execute(client: Client, message: Message, args: any) {
     console.log(
-      `>>>Remove user: ${message.author.username} in guild: ${message.guild?.name}`
+      `>>>Move user: ${message.author.username} in guild: ${message.guild?.name}`
     );
     if (!message.guild?.id) {
       return;
     }
-    if (message.guild?.id && hasPlayer(message.guild?.id) && args.length == 1) {
+    const length = playerGetQueue(message.guild?.id).length;
+    if (message.guild?.id && hasPlayer(message.guild?.id) && args.length == 1 && parseInt(args[0]) - 1 <= length && parseInt(args[0]) > 0) {
         emojiReact({message,emoji:"ok_hand"});
         playerRemove(message.guild?.id, parseInt(args[0]));
     } else {
